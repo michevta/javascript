e4425ffb5abf399f92fdaed26f3551198c3b302c
Michell Pinheiro de Carvalho <119219810+michelldecarvalho@users.noreply.github.com>
2023-06-24 12:34:14 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h `)
+if (vel > 60) { //Condição Simples
+    console.log('Você utrapassou a velocidade permitida MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+
+
+
+
